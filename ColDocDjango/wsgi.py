7e9f3f122c649b981be7335fc4ed8bf0634fa193
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-01-02 16:16:00 +0100

    some work on wsgi.py

--- ColDocDjango/wsgi.py
+++ ColDocDjango/wsgi.py
@@ -7,7 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+if False:
+    for j in ('','.'):
+        if j in sys.path:
+            sys.stderr.write('Warning: deleting %r from sys.path\n',j)
+            del sys.path[sys.path.index(j)]
+
+if True:
+    os.environ['COLDOC_SITE_ROOT']=os.path.dirname(__file__)
+
+if False:
+    a = os.path.realpath(__file__)
+    a = os.readlink(a)
+    a = os.path.realpath(a)
+    a = os.path.dirname(a)
+    a = os.path.dirname(a)
+    assert os.path.isdir(a), a
+    if a not in sys.path:
+        sys.path.insert(0, a)
+    del a
 
 from django.core.wsgi import get_wsgi_application
 
