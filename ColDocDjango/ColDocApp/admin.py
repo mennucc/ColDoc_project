7b25adc2c1364e969eee3f5480ded585266cddf1
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-05-03 23:02:37 +0200

    addendum for "move all users-related code to ColDocDjango.users"

--- ColDocDjango/ColDocApp/admin.py
+++ ColDocDjango/ColDocApp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import DColDoc , ColDocUser
+from .models import DColDoc
 
 admin.site.register(DColDoc)
 
+# TODO maybe this is not the best place for this action
+from django.contrib.auth.admin import UserAdmin
+from ColDocDjango.users import ColDocUser
 admin.site.register(ColDocUser, UserAdmin)
