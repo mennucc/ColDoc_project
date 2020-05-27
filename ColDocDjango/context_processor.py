5e94ee459e2c534925b49b333992ef24a8d87e9a
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-05-27 18:45:47 +0200

    add a context processor to pass USE_ALLAUTH to templates

---
+++ ColDocDjango/context_processor.py
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/a/433209
+
+from django.conf import settings # import the settings file
+
+def add_settings(request):
+    # return the value you want as a dictionnary. you may add multiple values in there.
+    return {'USE_ALLAUTH': settings.USE_ALLAUTH,
+            }
--- ColDocDjango/settings.py
+++ ColDocDjango/settings.py
@@ -129,6 +129,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'ColDocDjango.context_processor.add_settings',
             ],
         },
     },
