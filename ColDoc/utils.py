d0b9cdaa43db4ee2d478cb5677417943df45836d
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-10-08 14:45:55 +0200

    recurse_tree() : do not use mutables for defaults

--- ColDoc/utils.py
+++ ColDoc/utils.py
@@ -733,10 +733,22 @@ def prepare_anon_tree(coldoc_dir, uuid=None, lang=None,
 ############################
 
 
-def recurse_tree(coldoc_nick, blobs_dir, metadata_class, action, uuid='001', seen=set(), branch=[]):
+def recurse_tree(coldoc_nick, blobs_dir, metadata_class, action, uuid='001', seen=None, branch=None):
     """ recurse tree starting from `uuid` , for each node call 
     `action(uuid=uuid, metadata=metadata, branch=branch)` which should return a boolean.
     `recurse_tree` will return `and` of all return values of `action`"""
+    if seen == None:
+        seen = set()
+    if branch is None:
+        branch = []
+    assert isinstance(seen, set)
+    assert isinstance(branch, list)
+    return recurse_tree__(coldoc_nick, blobs_dir, metadata_class, action, uuid, seen, branch)
+
+
+def recurse_tree__(coldoc_nick, blobs_dir, metadata_class, action, uuid, seen, branch):
+    """ you may want to see `recurse tree`"""
+    #
     import ColDoc
     if isinstance(metadata_class,dict):
         metadata = metadata_class[uuid]
@@ -760,7 +772,7 @@ def recurse_tree(coldoc_nick, blobs_dir, metadata_class, action, uuid='001', see
         for u in metadata.get('child_uuid'):
             ## disabled, to speed up
             #logger.debug('moving down '+('→'*len(branch))+'from node %r to node %r',uuid,u)
-            r = recurse_tree(coldoc_nick, blobs_dir, metadata_class, action, uuid=u, seen=seen, branch=b)
+            r = recurse_tree__(coldoc_nick, blobs_dir, metadata_class, action, uuid=u, seen=seen, branch=b)
             ret = ret and r
     else:
         logger.warning("skipping duplicate node %r", uuid)
