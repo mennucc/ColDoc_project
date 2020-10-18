9580ff8db57213ab03af15e26dd7777f9ea024d7
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-10-18 16:08:33 +0200

    show UUID_Tree_Edge in administrative interface for coldoc

--- ColDocDjango/ColDocApp/admin.py
+++ ColDocDjango/ColDocApp/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import DColDoc
+from ColDocDjango.UUID.models import UUID_Tree_Edge
 
+class UUID_Tree_Edge_Admin(admin.TabularInline):
+    model = UUID_Tree_Edge
+    readonly_fields = ('coldoc','parent','child','child_ordering')
 
 class DColDocAdmin(admin.ModelAdmin):
     model = DColDoc
+    inlines = [
+        UUID_Tree_Edge_Admin,
+    ]    
     readonly_fields = ('root_uuid',)
 
 admin.site.register(DColDoc,DColDocAdmin)
