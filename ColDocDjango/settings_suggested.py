7cc51e15fd8e23f388a0cfd909fc4285ac2747c5
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-11-15 18:36:48 +0100

    do not comment in settings_suggested.py

--- ColDocDjango/settings_suggested.py
+++ ColDocDjango/settings_suggested.py
@@ -1,25 +1,26 @@
 ### these are some settings that are suggested 
 
-# MAIL_HOST = "smtp.server"
-# EMAIL_PORT = "587"
-# EMAIL_HOST_USER = "username"
-# EMAIL_HOST_PASSWORD = "password"
-# EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = "Helpdesk <helpdesk@that_email>"
+MAIL_HOST = "smtp.server"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = "username"
+EMAIL_HOST_PASSWORD = "password"
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "Helpdesk <helpdesk@that_email>"
 
 
 ## as per https://bugs.freedesktop.org/show_bug.cgi?id=5455
 
-#import mimetypes
-#for j in ('.gplt','.gnuplot'):
-#    mimetypes.add_type('application/x-gnuplot',j)
+import mimetypes
+for j in ('.gplt','.gnuplot'):
+    mimetypes.add_type('application/x-gnuplot',j)
 
 ## Google analytics
 
-# GOOGLE_SITE_VERIFICATION = 'googleXXXXXXXXXXXXXXXX.html'
+GOOGLE_SITE_VERIFICATION = 'googleXXXXXXXXXXXXXXXX.html'
 
 # you can add analytics by copying the template `analytics.html` in your deployed site and editing it
 # a template for Google analytics 4 can be activated by entering the G-code below
 
-# GOOGLE_ANALYTICS4 = 'G-xxxxxxxxxx'
+GOOGLE_ANALYTICS4 = 'G-xxxxxxxxxx'
+
 
