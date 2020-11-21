8f2d73eec627350cf2253ece1eada6da377fc102
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-11-21 16:16:35 +0100

    add useful imports and defines to ColDocDjango.views

--- ColDocDjango/views.py
+++ ColDocDjango/views.py
@@ -1,7 +1,27 @@
+import re, http
+
+number_re = re.compile(r'^[0-9]+\Z')
+
+import logging
+logger = logging.getLogger(__name__)
+
+from django.contrib.auth.validators import UnicodeUsernameValidator
+valid_user_re = UnicodeUsernameValidator().regex
+
+import django
+from django import forms
 from django.template.loader import render_to_string
-from django.shortcuts import get_object_or_404, render
+from django.core.exceptions import SuspiciousOperation
+from django.shortcuts import get_object_or_404, render, redirect
 from django.conf import settings
 from django.http import HttpResponse
+from django.contrib import messages
+from django import forms
+from django.contrib.auth import get_user_model
+
+UsMo = get_user_model()
+
+from ColDoc.utils import slug_re, slugp_re
 
 from ColDocDjango.ColDocApp.models import DColDoc
 
