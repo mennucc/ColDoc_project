175363c467df9a94c8d6294642f2e59afb4fd490
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-12-09 17:22:05 +0100

    mv javascript code to blobeditform.js

--- ColDocDjango/UUID/static/UUID/js/blobeditform.js
+++ ColDocDjango/UUID/static/UUID/js/blobeditform.js
@@ -53,3 +53,37 @@ function hide_and_show(){
 
 window.addEventListener('ready', hide_and_show() );
 
+////////////////////
+// https://stackoverflow.com/a/18170009/5058564
+// https://stackoverflow.com/a/32749586/5058564
+$("#id_blobeditform_save_no_reload").click(function() {
+  if (! check_primary_tab() ) {
+     return false;
+  }
+  
+  update_editform();
+  let blobeditform = document.getElementById("id_form_blobeditform");
+   // serializes the form's elements.
+  data = $("#id_form_blobeditform").serializeArray();
+  // add fake button press
+  data = data.concat([ {name: "save_no_reload", value: "save_no_reload"},]);
+  // post form
+  $.ajax(blobeditform.action, {
+	   type: "POST",
+	   url: blobeditform.action,
+	   data: data,
+	   success: function(response)  {
+	       msg = response['message'];
+	       if ( msg ) {
+		 alert(msg); 
+	       }
+	       if ( 'blobdiff' in response ) {
+		 blobdiff = response['blobdiff'];
+		 blobdiffdiv = document.getElementById("id_blob_diff");
+		 blobdiffdiv.innerHTML = blobdiff;
+	       }
+	   }
+	 });
+   // avoid to execute the actual submit of the form.
+    return false;
+});
