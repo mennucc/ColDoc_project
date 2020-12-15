1b0ca609d2382636c0496eaae05cce8850c9e8bd
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-12-15 10:58:21 +0100

    wrap blobdiff in <code> so that MathJax ignores it

--- ColDocDjango/UUID/static/UUID/js/blobeditform.js
+++ ColDocDjango/UUID/static/UUID/js/blobeditform.js
@@ -115,7 +115,7 @@ function blob_save_no_reload() {
 	       if ( 'blobdiff' in response ) {
 		 blobdiff = response['blobdiff'];
 		 blobdiffdiv = document.getElementById("id_blob_diff");
-		 blobdiffdiv.innerHTML = blobdiff;
+		 blobdiffdiv.innerHTML = '<code>' + blobdiff + '</code>';
 	       } else { console.log("Did not get blobdiff"); }
 	       if ( 'blob_md5' in response ) {
 		  blob_md5 = response['blob_md5'];
