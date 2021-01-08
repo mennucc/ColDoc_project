ea0dc13d0c373091155557ddd1d09d077a1b5136
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2021-01-08 21:21:15 +0100

    fix wsgi.py

--- ColDocDjango/wsgi.py
+++ ColDocDjango/wsgi.py
@@ -9,25 +9,45 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os, sys
 
+import logging
+logger = logging.getLogger(__name__)
+
 if False:
     for j in ('','.'):
         if j in sys.path:
             sys.stderr.write('Warning: deleting %r from sys.path\n',j)
             del sys.path[sys.path.index(j)]
 
-if True:
-    os.environ['COLDOC_SITE_ROOT']=os.path.dirname(__file__)
 
-if False:
-    a = os.path.realpath(__file__)
-    a = os.readlink(a)
-    a = os.path.realpath(a)
-    a = os.path.dirname(a)
-    a = os.path.dirname(a)
-    assert os.path.isdir(a), a
-    if a not in sys.path:
-        sys.path.insert(0, a)
-    del a
+if True:
+    a = __file__
+    if os.path.islink(a):
+        d = os.path.dirname(a)
+        if 'COLDOC_SITE_ROOT' in os.environ and not os.path.samefile( os.environ['COLDOC_SITE_ROOT'] , d ):
+            logger.error('Environment is COLDOC_SITE_ROOT=%r but wsgi script is in %r',
+                         os.environ['COLDOC_SITE_ROOT'], d)
+        else:
+            logger.info('COLDOC_SITE_ROOT=%r',d)
+            os.environ['COLDOC_SITE_ROOT'] = d
+        a = os.readlink(a)
+        a = os.path.realpath(a)
+        a = os.path.dirname(a)
+        sd = os.path.dirname(a)
+        if 'COLDOC_SRC_ROOT' in os.environ and not  os.path.samefile( os.environ['COLDOC_SRC_ROOT'], sd):
+            logger.error('Environment is COLDOC_SRC_ROOT=%r but wsgi script links to %r',
+                         os.environ['COLDOC_SRC_ROOT'], sd)
+        else:
+            os.environ['COLDOC_SRC_ROOT'] = sd
+            logger.info('COLDOC_SRC_ROOT=%r', sd)
+        if sd not in sys.path:
+            sys.path.insert(0, sd)
+    else:
+        if 'COLDOC_SITE_ROOT' not in os.environ:
+            logger.error('COLDOC_SITE_ROOT not known')
+        if 'COLDOC_SRC_ROOT' not in os.environ:
+            logger.error('COLDOC_SRC_ROOT not known')
+        else:
+            sys.path.insert(0, os.environ['COLDOC_SRC_ROOT'])
 
 from django.core.wsgi import get_wsgi_application
 
