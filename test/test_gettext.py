bf5717e6cfc849a2e9a13c1bd36ce01d67e8ce51
Andrea C. G. Mennucci <andrea.mennucci@gmail.com>
2022-03-19 21:56:22 +0100

    test_gettext , test format_lazy

--- test/test_gettext.py
+++ test/test_gettext.py
@@ -15,11 +15,17 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ColDocDjango.settings')
 django.setup()
 
 from django.utils.translation import gettext, gettext_lazy, gettext_noop, activate
+from django.utils.text import format_lazy
 
-print('fine')
+print('here a translation')
 a = gettext_lazy('help')
 print('and it is lazy ',type(a))
 
+m = ' ... %s ...' % a
+print('and it is NOT lazy ',type(m))
+
+m = format_lazy('{}\n', a)
+print('format_lazy is lazy ',type(m))
 
 print('fine')
 a = gettext_lazy('help') + 'here'
@@ -28,10 +34,10 @@ print('but now it is not lazy it is string ',type(a))
 
 print('crashes in django 3')
 a = 'here' + gettext_lazy('help')
-print('and it is ',type(a))
+print('and it is NOT lazy ',type(a))
 
 
 print('crashes in django 3')
 a = gettext_lazy('here') + gettext_lazy('help')
-print('and it is ',type(a))
+print('and it is NOT lazy',type(a))
 
