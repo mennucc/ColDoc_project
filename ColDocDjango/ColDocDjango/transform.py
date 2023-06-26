mennucc/ColDoc_project

commit 64e4e2ca02eed69ba21dc8b5a42e0b827f8fd8c1

squash_helper_ref : better query

ColDocDjango/ColDocDjango/transform.py
@@ -28,9 +28,9 @@ class squash_helper_ref(transform.squash_input_uuid):
         blob = None
         key = None
         logger.debug('searching label %r',label)
-        for j in ExtraMetadata.objects.filter(value=label):
-            if j.blob.coldoc == self.coldoc and j.key.endswith('M_label') and\
-               self.metadata.uuid != j.blob.uuid : # no need to rewrite refs inside the same blob
+        uuid = self.metadata.uuid
+        for j in ExtraMetadata.objects.filter(value=label, blob__uuid=uuid, blob__coldoc = self.coldoc, key__endswith = 'M_label' ):
+            if self.metadata.uuid != j.blob.uuid : # no need to rewrite refs inside the same blob
                 if blob is not None:
                     logger.warning('duplicate label %r in %r and in %r',
                                    label,blob,j.blob)

commit 6fa1431429efb6fdc2f80064d6aec2360e9a0e3c

squash_helper_ref : fix bug introduced in 64e4e2ca02eed6

ColDocDjango/ColDocDjango/transform.py
@@ -29,7 +29,7 @@ class squash_helper_ref(transform.squash_input_uuid):
         key = None
         logger.debug('searching label %r',label)
         uuid = self.metadata.uuid
-        for j in ExtraMetadata.objects.filter(value=label, blob__uuid=uuid, blob__coldoc = self.coldoc, key__endswith = 'M_label' ):
+        for j in ExtraMetadata.objects.filter(value=label, blob__coldoc = self.coldoc, key__endswith = 'M_label' ):
             if self.metadata.uuid != j.blob.uuid : # no need to rewrite refs inside the same blob
                 if blob is not None:
                     logger.warning('duplicate label %r in %r and in %r',