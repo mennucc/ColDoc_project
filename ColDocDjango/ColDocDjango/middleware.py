effccf1699217cd81f085c855002e8c41ac69f2a
Andrea C. G. Mennucci <andrea.mennucci@gmail.com>
2023-07-21 16:27:26 +0200

    add new locale middleware

--- ColDocDjango/ColDocDjango/middleware.py
+++ ColDocDjango/ColDocDjango/middleware.py
@@ -1,3 +1,11 @@
+from django.middleware.locale import LocaleMiddleware as dLM
+from django.utils import translation
+from django.conf import settings
+
+
+import logging
+logger = logging.getLogger(__name__)
+
 
 try:
     from django.utils.deprecation import MiddlewareMixin
@@ -28,3 +36,31 @@ class RedirectMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
         if isinstance(exception, RedirectException):
             return shortcuts.redirect(exception.url, exception.permanent)
+
+
+###############
+
+
+class LocaleMiddleware(dLM):
+    """ wrapper for django locale middleware, to cover bots and `lang=` """
+
+    def process_request(self, request):
+        """ search engine bots do not use  HTTP_ACCEPT_LANGUAGE """
+        accept = request.META.get("HTTP_ACCEPT_LANGUAGE")
+        cookie = request.COOKIES.get(settings.LANGUAGE_COOKIE_NAME)
+        if accept is None and cookie is None:
+            # import here to avoid circular dependency
+            from ColDocDjango.utils import iso3lang2iso2
+            from ColDoc.utils import lang_re
+            #
+            lang = request.GET.get('lang')
+            if lang and lang_re.match(lang):
+                lang2 = iso3lang2iso2(lang)
+                if lang2:
+                    translation.activate(lang2)
+                    request.LANGUAGE_CODE = translation.get_language()
+                    logger.debug('bot request %r (?) setting lang to %r -> %r',
+                                 request.META.get("HTTP_USER_AGENT"), lang, request.LANGUAGE_CODE)
+                    return
+        return super().process_request(request)
+
