9d387f755ffc21b87fd19af525b3ba612ece0233
Andrea C. G. Mennucci <andrea.mennucci@gmail.com>
2023-07-27 19:55:01 +0200

    install_CodeMirror.sh : install CodeMirror 5, fix bug

--- bin/install_CodeMirror.sh
+++ bin/install_CodeMirror.sh
@@ -5,13 +5,18 @@ set -e
 cd $( dirname $( realpath $0) )
 cd ..
 
+if ! test -d ColDocDjango/UUID ; then
+    echo RUNTIME ERROR
+    exit 1
+fi
+
 if ! test -d node_modules ; then mkdir node_modules  ; fi
 
 
 
 if which npm ; then
     echo Install CodeMirror using npm
-    npm install codemirror
+    npm install codemirror@5
 else
     cd node_modules
     if ! test -f codemirror.zip ; then
@@ -27,7 +32,6 @@ else
     done
 fi
 
-cd ..
 
 if ! test -L ColDocDjango/UUID/static/UUID/cm ; then
     ln -v -s -T  ../../../../node_modules/codemirror ColDocDjango/UUID/static/UUID/cm
