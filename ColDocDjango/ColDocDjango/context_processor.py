1948a1e295f31940a2700464b5c0c6380310ff7c
Andrea C. G. Mennucci <andrea.mennucci@gmail.com>
2023-08-09 21:12:10 +0200

    define session variables in context processor

--- ColDocDjango/ColDocDjango/context_processor.py
+++ ColDocDjango/ColDocDjango/context_processor.py
@@ -1,12 +1,22 @@
 # https://stackoverflow.com/a/433209
 
+import time
+
 from django.conf import settings # import the settings file
 
 def add_settings(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
+    if not request.user.is_anonymous:
+        session_expiry_age = request.session.get_expiry_age()
+        session_expiry_time = session_expiry_age + int(time.time())
+    else:
+        session_expiry_age = -1
+        session_expiry_time = -1
     return {'USE_ALLAUTH': settings.USE_ALLAUTH,
             'USE_BACKGROUND_TASKS': settings.USE_BACKGROUND_TASKS,
             'USE_WALLET': settings.USE_WALLET,
             'USE_CODEMIRROR': settings.USE_CODEMIRROR,
             'GOOGLE_ANALYTICS4' : settings.GOOGLE_ANALYTICS4,
+            'session_expiry_age' : session_expiry_age,
+            'session_expiry_time': session_expiry_time,
             }
--- ColDocDjango/UUID/views.py
+++ ColDocDjango/UUID/views.py
@@ -2276,12 +2276,8 @@ def index(request, NICK, UUID):
     if not request.user.is_anonymous:
         a = metadata.access
         access_icon  = get_access_icon(a)
-        session_expiry_age = request.session.get_expiry_age()
-        session_expiry_time = session_expiry_age + int(time.time())
     else:
         access_icon = ''
-        session_expiry_age = -1
-        session_expiry_time = -1
     ########################################## permission management
     #
     request.user.associate_coldoc_blob_for_has_perm(metadata.coldoc, metadata)
